66dfd64381f8edc93b53da7a346a3c8ea0f5c777
Emiel van de Laar <gemiel@gmail.com>
2020-07-09 21:04:29 +0200

    WIP

--- src/Handler/Packages.hs
+++ src/Handler/Packages.hs
@@ -1,27 +1,35 @@
 module Handler.Packages where
 
-import Import
-import Text.Julius (rawJS)
-import Text.Blaze (ToMarkup, toMarkup)
-import qualified Data.Char as Char
-import Data.Version
-import qualified Data.Text as T
-import qualified Data.Text.Lazy as TL
-import qualified Language.PureScript.Docs as D
-import Web.Bower.PackageMeta (PackageName, runPackageName, bowerDependencies, bowerLicense)
-import qualified Data.HashMap.Strict as HashMap
 import qualified Data.Aeson as Aeson
 import qualified Data.Aeson.BetterErrors as ABE
+import qualified Data.Char as Char
+import qualified Data.HashMap.Strict as HashMap
+import qualified Data.Text as T
+import qualified Data.Text.Lazy as TL
+import Data.Version
+import Import
 import qualified Language.PureScript as P
+import qualified Language.PureScript.Docs as D
+import Text.Blaze (ToMarkup, toMarkup)
+import Text.Julius (rawJS)
+import Web.Bower.PackageMeta
+  ( PackageName
+  , bowerDependencies
+  , bowerLicense
+  , runPackageName
+  )
 
-import SearchIndex (isDeprecated)
-import Handler.Database
-import Handler.Caching
-import Handler.Utils
-import TemplateHelpers
 import qualified GithubAPI
+import Handler.Caching
+import Handler.Database
+import Handler.Utils
+import SearchIndex (isDeprecated)
+import TemplateHelpers
 
-newtype FirstLetter = FirstLetter { getFirstLetter :: Char }
+newtype FirstLetter =
+  FirstLetter
+    { getFirstLetter :: Char
+    }
 
 instance ToMarkup FirstLetter where
   toMarkup (FirstLetter a)
@@ -40,12 +48,10 @@ getHomeR =
     latest <- getLatestPackages
     let firstLetter :: PackageName -> Maybe FirstLetter
         firstLetter = fmap FirstLetter . headMay . stripIntro . runPackageName
-
         stripIntro :: Text -> Text
         stripIntro s = fromMaybe s (stripPrefix "purescript-" s)
-
         pkgNamesByLetter :: [[PackageName]]
-        pkgNamesByLetter = groupBy ((==) `on` (firstLetter )) pkgNames
+        pkgNamesByLetter = groupBy ((==) `on` (firstLetter)) pkgNames
     defaultLayout $(widgetFile "homepage")
 
 latestVersionOr404 :: PackageName -> Handler Version
@@ -70,45 +76,48 @@ getPackageAvailableVersionsR (PathPackageName pkgName) =
   cacheJSON $ do
     renderUrl <- getUrlRender
     vs <- availableVersionsFor pkgName
-    let toPair v = [ toJSON $ showVersion v
-                   , toJSON $ renderUrl $ alternateVersionUrl v
-                   ]
+    let toPair v =
+          [toJSON $ showVersion v, toJSON $ renderUrl $ alternateVersionUrl v]
     return $ toJSON $ map toPair $ sort vs
   where
-  alternateVersionUrl v = PackageVersionR (PathPackageName pkgName) (PathVersion v)
+    alternateVersionUrl v =
+      PackageVersionR (PathPackageName pkgName) (PathVersion v)
 
 getPackageVersionR :: PathPackageName -> PathVersion -> Handler Html
 getPackageVersionR (PathPackageName pkgName) (PathVersion version) =
   cacheHtmlConditional $
-    findPackageWithLatest pkgName version $ \pkg@D.Package{..} latestPkg -> do
-      moduleList <- renderModuleList pkg
-      ereadme    <- tryGetReadme pkg
-      let cacheStatus = either (const NotOkToCache) (const OkToCache) ereadme
-      content <- defaultLayout $ do
+  findPackageWithLatest pkgName version $ \pkg@D.Package {..} latestPkg -> do
+    moduleList <- renderModuleList pkg
+    ereadme <- tryGetReadme pkg
+    let cacheStatus = either (const NotOkToCache) (const OkToCache) ereadme
+    content <-
+      defaultLayout $ do
         setTitle (toHtml (runPackageName pkgName))
+        let isLatest = version == D.pkgVersion latestPkg
         let dependencies = bowerDependencies pkgMeta
         let deprecated = isDeprecated latestPkg
         $(widgetFile "packageVersion")
-      return (cacheStatus, content)
+    return (cacheStatus, content)
+
 
 isPurescriptPackage :: PackageName -> Bool
 isPurescriptPackage pkgName =
-    "purescript-" `T.isPrefixOf` runPackageName pkgName
+  "purescript-" `T.isPrefixOf` runPackageName pkgName
 
 getPackageIndexR :: Handler TypedContent
 getPackageIndexR =
   selectRep $ do
     provideRep (redirect HomeR :: Handler Html)
     provideRep . cacheText . map unlines $ pkgNames
-    provideRep . cacheJSON . map toJSON  $ pkgNames
+    provideRep . cacheJSON . map toJSON $ pkgNames
   where
-  pkgNames :: Handler [TL.Text]
-  pkgNames = map (fromStrict . runPackageName) <$> getAllPackageNames
+    pkgNames :: Handler [TL.Text]
+    pkgNames = map (fromStrict . runPackageName) <$> getAllPackageNames
 
 postPackageIndexR :: Handler Value
 postPackageIndexR = do
   package <- getUploadedPackageFromBody
-  mtoken  <- lookupAuthTokenHeader
+  mtoken <- lookupAuthTokenHeader
   case mtoken of
     Nothing -> notAuthenticated
     Just token -> do
@@ -116,49 +125,42 @@ postPackageIndexR = do
       let package' = D.verifyPackage user package
       insertPackage package'
       sendResponseCreated $ packageRoute package'
-
   where
-  getUploadedPackageFromBody = do
-    ejson <- parseJsonBodyPotentiallyGzipped
-    case ejson of
-      Left err ->
-        badRequest (pack err)
-      Right json -> do
-        epackage <- parseUploadedPackage json
-        case epackage of
-          Left err ->
-            let errorMessage = unlines $ displayJsonError json err
-            in badRequest errorMessage
-          Right package ->
-            return package
-
-  getUserOrNotAuthenticated token = do
-    euser <- GithubAPI.getUser token
-    case euser of
-      Left err ->
-        $logError (tshow err) >> internalServerError
-      Right Nothing ->
-        notAuthenticated
-      Right (Just user) ->
-        return user
-
-  lookupAuthTokenHeader = do
-    mheader <- lookupHeader "Authorization"
-    return $ mheader >>= extractToken
-
-  extractToken header =
-    case words (decodeUtf8 header) of
-      ["token", token] -> Just $ GithubAuthToken $ encodeUtf8 token
-      _ -> Nothing
+    getUploadedPackageFromBody = do
+      ejson <- parseJsonBodyPotentiallyGzipped
+      case ejson of
+        Left err -> badRequest (pack err)
+        Right json -> do
+          epackage <- parseUploadedPackage json
+          case epackage of
+            Left err ->
+              let errorMessage = unlines $ displayJsonError json err
+               in badRequest errorMessage
+            Right package -> return package
+    getUserOrNotAuthenticated token = do
+      euser <- GithubAPI.getUser token
+      case euser of
+        Left err -> $logError (tshow err) >> internalServerError
+        Right Nothing -> notAuthenticated
+        Right (Just user) -> return user
+    lookupAuthTokenHeader = do
+      mheader <- lookupHeader "Authorization"
+      return $ mheader >>= extractToken
+    extractToken header =
+      case words (decodeUtf8 header) of
+        ["token", token] -> Just $ GithubAuthToken $ encodeUtf8 token
+        _ -> Nothing
 
 getPackageVersionDocsR :: PathPackageName -> PathVersion -> Handler Html
 getPackageVersionDocsR (PathPackageName pkgName) (PathVersion version) =
-  findPackage pkgName version $ \pkg@D.Package{..} ->
+  findPackage pkgName version $ \pkg@D.Package {..} ->
     redirect (packageRoute pkg)
 
-getPackageVersionModuleDocsR :: PathPackageName -> PathVersion -> Text -> Handler Html
+getPackageVersionModuleDocsR ::
+     PathPackageName -> PathVersion -> Text -> Handler Html
 getPackageVersionModuleDocsR (PathPackageName pkgName) (PathVersion version) mnString =
-  cacheHtml $ findPackageWithLatest pkgName version $ \pkg@D.Package{..} latestPkg -> do
+  cacheHtml $
+  findPackageWithLatest pkgName version $ \pkg@D.Package {..} latestPkg -> do
     moduleList <- renderModuleList pkg
     mhtmlDocs <- renderHtmlDocs pkg mnString
     case mhtmlDocs of
@@ -172,15 +174,13 @@ getPackageVersionModuleDocsR (PathPackageName pkgName) (PathVersion version) mnS
 
 getBuiltinDocsR :: Text -> Handler Html
 getBuiltinDocsR mnString =
-  let
-    mn = P.moduleNameFromString mnString
-  in
-    case primDocsFor mn of
-      Just htmlDocs -> do
-        moduleList <- builtinModuleList
-        defaultLayout $ do
-          setTitle (toHtml mnString)
-          [whamlet|
+  let mn = P.moduleNameFromString mnString
+   in case primDocsFor mn of
+        Just htmlDocs -> do
+          moduleList <- builtinModuleList
+          defaultLayout $ do
+            setTitle (toHtml mnString)
+            [whamlet|
             <div .page-title.clearfix>
               <div .page-title__label>Module
               <h1 .page-title__title>#{insertBreaks mn}
@@ -191,30 +191,27 @@ getBuiltinDocsR mnString =
             <div .col.col--aside>
               #{moduleList}
             |]
-      Nothing ->
-        defaultLayout404 $ [whamlet|
+        Nothing ->
+          defaultLayout404 $
+          [whamlet|
           <h2>Module not found
           <p>No such builtin module: #
             <b>#{mnString}
           |]
 
-findPackage ::
-  PackageName ->
-  Version ->
-  (D.VerifiedPackage -> Handler r) ->
-  Handler r
+findPackage :: PackageName -> Version -> (D.VerifiedPackage -> Handler r) -> Handler r
 findPackage pkgName version cont = do
   pkg' <- lookupPackage pkgName version
   case pkg' of
-    Right pkg -> cont pkg
     Left NoSuchPackage -> packageNotFound pkgName
     Left NoSuchPackageVersion -> packageVersionNotFound pkgName version
+    Right pkg -> cont pkg
 
 findPackageWithLatest ::
-  PackageName ->
-  Version ->
-  (D.VerifiedPackage -> D.VerifiedPackage -> Handler r) ->
-  Handler r
+     PackageName
+  -> Version
+  -> (D.VerifiedPackage -> D.VerifiedPackage -> Handler r)
+  -> Handler r
 findPackageWithLatest pkgName version cont = do
   findPackage pkgName version $ \pkg -> do
     latestVersion <- fromMaybe version <$> getLatestVersionFor pkgName
@@ -243,53 +240,47 @@ versionSelector pkgName version = do
 
 -- | Try to parse a D.UploadedPackage from a JSON Value.
 parseUploadedPackage ::
-  Value ->
-  Handler (Either (ABE.ParseError D.PackageError) D.UploadedPackage)
+     Value -> Handler (Either (ABE.ParseError D.PackageError) D.UploadedPackage)
 parseUploadedPackage value = do
   minVersion <- appMinimumCompilerVersion . appSettings <$> getYesod
   return $ ABE.parseValue (D.asUploadedPackage minVersion) value
 
 displayJsonError :: Value -> ABE.ParseError D.PackageError -> [Text]
-displayJsonError value e = case e of
-  ABE.InvalidJSON _ ->
-    ["The file you submitted was not valid JSON."]
-  ABE.BadSchema _ _ ->
-    ABE.displayError D.displayPackageError e ++ extraInfo
-
-  where
+displayJsonError value e =
+  case e of
+    ABE.InvalidJSON _ -> ["The file you submitted was not valid JSON."]
+    ABE.BadSchema _ _ -> ABE.displayError D.displayPackageError e ++ extraInfo
   -- Attempt to extract the compiler version that a JSON upload was created
   -- with.
-  extractVersion =
-    toObject
-    >=> HashMap.lookup "compilerVersion"
-    >=> toString
-    >=> (D.parseVersion' . unpack)
-
-  toObject json =
-    case json of
-      Aeson.Object obj -> Just obj
-      _ -> Nothing
-
-  toString json =
-    case json of
-      Aeson.String str -> Just str
-      _ -> Nothing
-
+  where
+    extractVersion =
+      toObject >=>
+      HashMap.lookup "compilerVersion" >=>
+      toString >=> (D.parseVersion' . unpack)
+    toObject json =
+      case json of
+        Aeson.Object obj -> Just obj
+        _ -> Nothing
+    toString json =
+      case json of
+        Aeson.String str -> Just str
+        _ -> Nothing
   -- Some extra information about what might have caused an error.
-  extraInfo =
-    case extractVersion value of
-      Just v | v > P.version ->
-        let pursuitVersion = pack (showVersion P.version) in
-        [ "Usually, this occurs because the JSON data was generated with a newer " <>
-          "version of the compiler than what Pursuit is currently using, and " <>
-          "the JSON format changed between compiler releases."
-        , "This data was generated with " <> pack (showVersion v) <> " of the compiler."
-        , "Pursuit is currently using " <> pursuitVersion <> "."
-        , "You might be able to fix this by temporarily downgrading to " <>
-           pursuitVersion <> " to generate the JSON data."
-        ]
-      _ ->
-        []
+    extraInfo =
+      case extractVersion value of
+        Just v
+          | v > P.version ->
+            let pursuitVersion = pack (showVersion P.version)
+             in [ "Usually, this occurs because the JSON data was generated with a newer " <>
+                  "version of the compiler than what Pursuit is currently using, and " <>
+                  "the JSON format changed between compiler releases."
+                , "This data was generated with " <>
+                  pack (showVersion v) <> " of the compiler."
+                , "Pursuit is currently using " <> pursuitVersion <> "."
+                , "You might be able to fix this by temporarily downgrading to " <>
+                  pursuitVersion <> " to generate the JSON data."
+                ]
+        _ -> []
 
 versionSeries :: Version -> String
 versionSeries (Version (0:x:_) _) = "0." ++ show x ++ ".x"
